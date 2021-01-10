db2a4cd7c5687c111ce505144105e6c0ce0ad3ea
alanskverer <59963384+alanskverer@users.noreply.github.com>
2021-01-10 17:42:55 +0200

    commit2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Your app!</Text>
+      <Text>exour app!</Text>
       <StatusBar style="auto" />
     </View>
   );
