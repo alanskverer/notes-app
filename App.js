4ab0309c9d0dcfa06d0272823ace389a8c7975b9
alanskverer <59963384+alanskverer@users.noreply.github.com>
2021-01-11 17:57:16 +0200

    Finished navigation

--- App.js
+++ App.js
@@ -3,15 +3,29 @@ import { View, Text, Image } from 'react-native'
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import Categories from './Components/Categories';
 import MyCard from './Components/MyCard';
+import Nav from './Components/Nav';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Categories />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Categories">
+        <Stack.Screen name="Categories" component={Categories} />
+        <Stack.Screen name="Nav" component={Nav} />
 
-    </View>
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+
   );
 }
--- Components/Nav.js
+++ Components/Nav.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Nav() {
+
+export default function Nav(props) {
+
+    useEffect(() => {
+
+        getData()
+    }, [])
+
+
+    const getData = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('Categories')
+            jsonValue != null ? JSON.parse(jsonValue) : null;
+            console.log(jsonValue);
+        } catch (e) {
+            // error reading value
+        }
+    }
+
+
+    let { userName } = props.route.params
     return (
         <View>
-            <Text>nav</Text>
+            <Text>Hello {userName}</Text>
         </View>
     )
 }
