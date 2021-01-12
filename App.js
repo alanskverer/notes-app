661dc339fd382cb397d8ea526df5c1548e49c8f7
alanskverer <59963384+alanskverer@users.noreply.github.com>
2021-01-12 19:40:49 +0200

    Finished Camera -  App Is Done

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native'
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import Categories from './Components/Categories';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Notes from './Components/Notes';
+import PickImage from './Components/PickImage';
 
 
 
@@ -18,6 +16,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Categories">
         <Stack.Screen name="Categories" component={Categories} />
         <Stack.Screen name="Notes" component={Notes} />
+        <Stack.Screen name="PickImage" component={PickImage} />
       </Stack.Navigator>
     </NavigationContainer>
 
